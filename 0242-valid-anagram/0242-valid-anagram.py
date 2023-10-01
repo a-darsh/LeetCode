32682fb95d86534ede9b0895d57e680f196e9f08
adarshr <102381596+a-darsh@users.noreply.github.com>
2023-10-01 12:55:55 -0500

    Time: 49 ms (80.56%), Space: 16.7 MB (95.73%) - LeetHub

--- 0242-valid-anagram/0242-valid-anagram.py
+++ 0242-valid-anagram/0242-valid-anagram.py
@@ -1,5 +1,26 @@
 class Solution:
     def isAnagram(self, s: str, t: str) -> bool:
-      return all(s.count(x) == t.count(x) for x in set(s+t))
         
+        if len(s) != len(t):
+          return False
         
+        hmap1 = {}
+        hmap2 = {}
+        
+        for c in s:
+          if c in hmap1:
+            hmap1[c] += 1
+          else:
+            hmap1[c] = 1
+            
+        for c in t:
+          if c in hmap2:
+            hmap2[c] += 1
+          else:
+            hmap2[c] = 1
+            
+        for k,v in hmap1.items():
+          if k not in hmap2 or hmap2[k] != v:
+            return False
+          
+        return True
