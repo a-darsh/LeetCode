834652b1643f763a8400677c35778ad0586417ee
adarshr <102381596+a-darsh@users.noreply.github.com>
2023-10-23 17:19:07 -0500

    Time: 38 ms (51.65%), Space: 16.1 MB (75.24%) - LeetHub

--- 0070-climbing-stairs/0070-climbing-stairs.py
+++ 0070-climbing-stairs/0070-climbing-stairs.py
@@ -1,20 +1,13 @@
 class Solution:
     def climbStairs(self, n: int) -> int:
+      one, two = 1, 1
       
-      wayList = []
-      k = 0 
-      for i in range(n,-1,-1):
+      for i in range(n-2, -1, -1):
+        temp  = one + two
+        two = one
+        one  = temp
         
-        if i == n or i==n-1:
-          wayList.append(1)
-          k += 1
-        
-        else:
-          wayList.append(wayList[k-1]+wayList[k-2])
-          k += 1
-          
-      
-      return wayList[k-1]
+      return one
           
         
         
