80b3fb74f7f008518b9db9f0b3b928853b566bc2
adarshr <102381596+a-darsh@users.noreply.github.com>
2023-10-24 16:08:31 -0500

    Time: 38 ms (60.76%), Space: 16.3 MB (35.36%) - LeetHub

--- 0062-unique-paths/0062-unique-paths.py
+++ 0062-unique-paths/0062-unique-paths.py
@@ -1,18 +1,36 @@
 class Solution:
-    def uniquePaths(self, m: int, n: int) -> int:
+    def uniquePaths(self, m: int, n: int, memo = {}) -> int:
+      
+      #top up
+      
+      if (m,n) in memo:
+        return memo[(m,n)]
+      
+      if m==1 and n==1:
+        memo[(1,1)] = 1
+        return 1
+      
+      if m==0 or n ==0:
+        return 0
+      
+      memo[(m,n)] = self.uniquePaths(m-1, n, memo) + self.uniquePaths(m, n-1, memo)
+      return memo[(m,n)]
+      
+      
   
-        row = [1]*(m)
+#         #bottom up
+#         row = [1]*(m)
         
-        newRow = [0]*(m)
+#         newRow = [0]*(m)
         
-        for i in range(n-2, -1, -1):
-          for j in range(m-1,-1,-1 ):
-            if j+1 <= m-1: 
-              newRow[j] = (row[j]+newRow[j+1])
-            else:
-              newRow[j] = (row[j])
+#         for i in range(n-2, -1, -1):
+#           for j in range(m-1,-1,-1 ):
+#             if j+1 <= m-1: 
+#               newRow[j] = (row[j]+newRow[j+1])
+#             else:
+#               newRow[j] = (row[j])
               
           
-          row = newRow
+#           row = newRow
           
-        return row[0]
+#         return row[0]
