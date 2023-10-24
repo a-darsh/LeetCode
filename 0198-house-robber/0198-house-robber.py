e4d0811952642849be1a014bb1c3002bf0ae5084
adarshr <102381596+a-darsh@users.noreply.github.com>
2023-10-23 21:33:57 -0500

    Time: 34 ms (87.39%), Space: 16.2 MB (80.82%) - LeetHub

--- 0198-house-robber/0198-house-robber.py
+++ 0198-house-robber/0198-house-robber.py
@@ -1,13 +1,14 @@
 class Solution:
     def rob(self, nums: List[int]) -> int:
         
-        rob1, rob2 = 0, 0
+        if len(nums) == 1:
+          return nums[0]
         
-        for n in nums:
+        rob1, rob2 = nums[-1], 0
+        
+        for i in range(len(nums)-2, -1, -1):
+          temp = max(rob1, nums[i]+rob2)
+          rob1, rob2 = temp, rob1
           
-          temp  = max(n + rob1, rob2)
-          rob1 = rob2
-          rob2 = temp
-          
-        return rob2
+        return rob1
       
