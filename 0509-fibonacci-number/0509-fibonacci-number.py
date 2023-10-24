daa4c1e1f905e9fdb24d498818f40482ee041ae0
adarshr <102381596+a-darsh@users.noreply.github.com>
2023-10-24 15:46:50 -0500

    Time: 34 ms (85.37%), Space: 16.2 MB (41.04%) - LeetHub

--- 0509-fibonacci-number/0509-fibonacci-number.py
+++ 0509-fibonacci-number/0509-fibonacci-number.py
@@ -1,17 +1,39 @@
 class Solution:
-    def fib(self, n: int) -> int:
+    def fib(self, n: int, memo = {}) -> int:
+      
+      #top down
+      
+      
+      if n in memo:
+        return memo[n]
+      
+      if n==0:
+        memo[n] = 0 
+        return 0
+      
+      if n==1:
+        memo[n] = 1
+        return 1
+      
+      memo[n] = self.fib(n-1, memo) + self.fib(n-2, memo)
+      return memo[n]
+      
+      
+      
+      
+      
+#         #bottom up - bottom is 0, top is n
         
-        #bottom up - bottom is 0, top is n
+#         one , two = 0,1
         
-        one , two = 0,1
-        
-        for i in range(2, n+1):
+#         for i in range(2, n+1):
           
-          temp = two
-          two = one + two
-          one  = temp
+#           temp = two
+#           two = one + two
+#           one  = temp
         
-        return two if n > 0 else one
+#         return two if n > 0 else one
+      
           
           
           
