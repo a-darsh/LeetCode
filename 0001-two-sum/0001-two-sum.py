a40ec1261319f8a7588bb41534b32514d52e805b
Adarsh Ramesh Kumar <102381596+a-darsh@users.noreply.github.com>
2024-03-02 14:13:35 -0600

    Time: 51 ms (93.53%), Space: 17.8 MB (41.56%) - LeetHub

--- 0001-two-sum/0001-two-sum.py
+++ 0001-two-sum/0001-two-sum.py
@@ -1,11 +1,11 @@
-class Solution(object):
-    def twoSum(self, nums, target):
+class Solution:
+    def twoSum(self, nums: List[int], target: int) -> List[int]:
         
         hmap = {}
         
-        for i,n in enumerate (nums):
-          
-          if (target-n) in hmap:
-            return [hmap[target-n],  i]
-          
-          hmap[n] = i
+        for i,num in enumerate(nums):
+            if target-num in hmap:
+                return [hmap[target-num], i]
+            hmap[num] = i
+            
+        
