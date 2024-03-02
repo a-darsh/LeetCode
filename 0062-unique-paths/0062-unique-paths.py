4dc24db1ab421718b9de23e383b89db8616adf97
Adarsh Ramesh Kumar <102381596+a-darsh@users.noreply.github.com>
2024-03-02 11:26:40 -0600

    Time: 42 ms (19.41%), Space: 16.7 MB (39.56%) - LeetHub

--- 0062-unique-paths/0062-unique-paths.py
+++ 0062-unique-paths/0062-unique-paths.py
@@ -1,36 +1,15 @@
 class Solution:
-    def uniquePaths(self, m: int, n: int, memo = {}) -> int:
-      
-      #top up
-      
-      if (m,n) in memo:
-        return memo[(m,n)]
-      
-      if m==1 and n==1:
-        memo[(1,1)] = 1
-        return 1
-      
-      if m==0 or n ==0:
-        return 0
-      
-      memo[(m,n)] = self.uniquePaths(m-1, n, memo) + self.uniquePaths(m, n-1, memo)
-      return memo[(m,n)]
-      
-      
-  
-#         #bottom up
-#         row = [1]*(m)
+    def uniquePaths(self, m: int, n: int, hmap = {}) -> int:
         
-#         newRow = [0]*(m)
+        if (m,n) in hmap:
+            return hmap[(m,n)]
         
-#         for i in range(n-2, -1, -1):
-#           for j in range(m-1,-1,-1 ):
-#             if j+1 <= m-1: 
-#               newRow[j] = (row[j]+newRow[j+1])
-#             else:
-#               newRow[j] = (row[j])
-              
-          
-#           row = newRow
-          
-#         return row[0]
+        if m==0 or n==0:
+            return 0
+        
+        if m==1 and n==1:
+            return 1
+        
+        hmap[(m,n)] = self.uniquePaths(m-1,n, hmap) + self.uniquePaths(m, n-1, hmap)
+        
+        return hmap[(m,n)]
