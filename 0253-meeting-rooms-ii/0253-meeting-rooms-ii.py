572a33ff9b064011cce8c4fe591d6dfd697d9262
Adarsh Ramesh Kumar <102381596+a-darsh@users.noreply.github.com>
2024-03-08 09:10:30 -0600

    Time: 70 ms (79.99%), Space: 20.2 MB (43.12%) - LeetHub

--- 0253-meeting-rooms-ii/0253-meeting-rooms-ii.py
+++ 0253-meeting-rooms-ii/0253-meeting-rooms-ii.py
@@ -3,23 +3,26 @@ import heapq
 class Solution:
     def minMeetingRooms(self, intervals: List[List[int]]) -> int:
         
-        stck = []
-        time = []
-        for i, j in intervals:
-            time.append((i,'s'))
-            time.append((j,'e'))
+        start, end = [], []
+        for i,j in intervals:
+            start.append(i)
+            end.append(j)
         
-        time.sort()
+        start.sort()
+        end.sort()
+        
+        s, e=0,0
         rooms = 0
         minRooms = 0
-        while(time):
-            ele = time.pop(0)[1]
-            if ele=='s':
+        while(s<len(start)):
+            if start[s] < end[e]:
+                s+=1
                 rooms+=1
-                minRooms = max(minRooms, rooms)
-            elif ele=='e':
+            else:
+                e+=1
                 rooms-=1
-        
+            minRooms = max(minRooms, rooms)
+            
         return minRooms
         
             
