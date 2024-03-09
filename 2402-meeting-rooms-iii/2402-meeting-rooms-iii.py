729dc71baf875e0c1466b1803cdafcf9e614589c
Adarsh Ramesh Kumar <102381596+a-darsh@users.noreply.github.com>
2024-03-09 13:41:12 -0600

    Time: 1172 ms (75.36%), Space: 62.1 MB (94.44%) - LeetHub

--- 2402-meeting-rooms-iii/2402-meeting-rooms-iii.py
+++ 2402-meeting-rooms-iii/2402-meeting-rooms-iii.py
@@ -1,30 +1,27 @@
-from heapq import heappush, heappop
-
 class Solution:
     def mostBooked(self, n: int, meetings: List[List[int]]) -> int:
-        meetings.sort()
-        available = list(range(n))
-        used = []  # Will store (end time, room index)
-        count = [0] * n
         
+        used = []
+        available = [i for i in range(n)]
+        count = [0]*n
+        
+        meetings.sort()
         for start, end in meetings:
-            # Free up rooms that are now available
             while used and used[0][0] <= start:
                 _, room = heappop(used)
                 heappush(available, room)
             
             if available:
-                # Use the first available room
                 room = heappop(available)
+                heappush(used, (end, room))
+                count[room] += 1
             else:
-                # Wait for the next room to be available
-                end_time, room = heappop(used)
-                # Adjust the meeting start and end times based on the room availability
-                end += end_time - start
-                start = end_time
-            
-            # Book the room
-            heappush(used, (end, room))
-            count[room] += 1
+                nextEnd, room = heappop(used)
+                end = end + nextEnd-start
+                heappush(used, (end, room))
+                count[room] += 1
         
         return count.index(max(count))
+        
+        
+        
