a50c30f250d1a9d1e509daae48900d62136cb3d4
Adarsh Ramesh Kumar <102381596+a-darsh@users.noreply.github.com>
2024-03-24 20:45:48 -0500

    Time: 564 ms (89.86%), Space: 0B (100.00%) - LeetHub

---
+++ 0626-exchange-seats/0626-exchange-seats.sql
@@ -0,0 +1,11 @@
+# Write your MySQL query statement below
+select
+case 
+    when mod(id,2)=0 then id-1
+    when id = (select max(id) from Seat) then id
+    else id+1
+end as id,
+student
+from Seat
+order by id asc
+
