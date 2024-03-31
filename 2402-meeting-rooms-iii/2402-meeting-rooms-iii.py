92d878f6030a8ba8a9a7fe2986c30d68a1bf7e6e
Adarsh Ramesh Kumar <102381596+a-darsh@users.noreply.github.com>
2024-03-31 16:09:53 -0500

    Time: 1191 ms (62.47%), Space: 63 MB (19.65%) - LeetHub

--- 2402-meeting-rooms-iii/2402-meeting-rooms-iii.py
+++ 2402-meeting-rooms-iii/2402-meeting-rooms-iii.py
@@ -1,27 +1,31 @@
+from heapq import heappop, heappush
+from typing import List
+
 class Solution:
     def mostBooked(self, n: int, meetings: List[List[int]]) -> int:
-        
-        used = []
-        available = [i for i in range(n)]
-        count = [0]*n
-        
-        meetings.sort()
+        meetings.sort()  # Ensure meetings are sorted by start time
+        free = [i for i in range(n)]
+        occup = []  # (endtime, room)
+        count = [0] * n
+
         for start, end in meetings:
-            while used and used[0][0] <= start:
-                _, room = heappop(used)
-                heappush(available, room)
-            
-            if available:
-                room = heappop(available)
-                heappush(used, (end, room))
+            # Free up rooms that have become available
+            while occup and occup[0][0] <= start:
+                _, room = heappop(occup)
+                heappush(free, room)
+
+            # Schedule the meeting in the soonest available room
+            if free:
+                room = heappop(free)
+                heappush(occup, (end, room))
                 count[room] += 1
             else:
-                nextEnd, room = heappop(used)
-                end = end + nextEnd-start
-                heappush(used, (end, room))
+                earlyEnd, room = heappop(occup)
+                newEnd = max(earlyEnd, start) + (end - start)  # Schedule after the current earliest end
+                heappush(occup, (newEnd, room))
                 count[room] += 1
-        
-        return count.index(max(count))
-        
-        
-        
+
+        # Find the room that hosted the most meetings
+        maxVal = max(count)
+        return count.index(maxVal)
+
