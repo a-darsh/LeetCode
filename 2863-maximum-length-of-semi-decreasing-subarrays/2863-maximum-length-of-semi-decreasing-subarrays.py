ebd936419d17922834c63651fb5a418546032563
Adarsh Ramesh Kumar <102381596+a-darsh@users.noreply.github.com>
2024-04-05 09:52:50 -0500

    Time: 733 ms (11.71%), Space: 37.8 MB (10.04%) - LeetHub

--- 2863-maximum-length-of-semi-decreasing-subarrays/2863-maximum-length-of-semi-decreasing-subarrays.py
+++ 2863-maximum-length-of-semi-decreasing-subarrays/2863-maximum-length-of-semi-decreasing-subarrays.py
@@ -1,10 +1,11 @@
 class Solution:
     def maxSubarrayLength(self, nums: List[int]) -> int:
-        stack = sorted([(x,i) for i,x in enumerate(nums)], reverse=True)
-        best  = 0
-        for i,x in enumerate(nums):
-            while stack and x>stack[-1][0]:
-                y,j = stack.pop()
-                best  = max(best, j-i+1)
         
-        return best
+        stack = sorted([(x,i) for i,x in enumerate(nums)], reverse=True)
+        maxLen = 0
+        for i,x in enumerate(nums):
+            while stack and stack[-1][0]<x:
+                y, j = stack.pop()
+                maxLen = max(maxLen, j-i + 1)
+        
+        return maxLen
