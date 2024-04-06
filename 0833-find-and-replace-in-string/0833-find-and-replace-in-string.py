1f39e1fdb49035041fcd4c8ac6761a8f3bb51c80
Adarsh Ramesh Kumar <102381596+a-darsh@users.noreply.github.com>
2024-04-06 12:14:54 -0500

    Time: 34 ms (92.44%), Space: 16.6 MB (92.32%) - LeetHub

--- 0833-find-and-replace-in-string/0833-find-and-replace-in-string.py
+++ 0833-find-and-replace-in-string/0833-find-and-replace-in-string.py
@@ -2,11 +2,10 @@ class Solution:
     def findReplaceString(self, s: str, indices: List[int], sources: List[str], targets: List[str]) -> str:
         
         newStr = s
-        sorted_combination = sorted(zip(indices, sources, targets), reverse=True)
-    
-        for idx, source, target in sorted_combination:
+        sortedComb = sorted(zip(indices, sources, targets), reverse=True)
+        for idx, source, target in sortedComb:
             if s[idx:idx+len(source)]==source:
-                newStr = newStr[:idx] + target + newStr[idx+len(source):]
+                newStr = newStr[:idx]+target+newStr[idx+len(source):]
         return newStr
             
         
