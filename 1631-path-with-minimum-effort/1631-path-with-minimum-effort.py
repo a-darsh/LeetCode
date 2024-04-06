be6db2fba15439d3fefbeee6dc33e981b9a75a28
Adarsh Ramesh Kumar <102381596+a-darsh@users.noreply.github.com>
2024-04-06 18:52:07 -0500

    Time: 500 ms (67.84%), Space: 18.9 MB (43.83%) - LeetHub

---
+++ 1631-path-with-minimum-effort/1631-path-with-minimum-effort.py
@@ -0,0 +1,24 @@
+class Solution:
+    def minimumEffortPath(self, heights: List[List[int]]) -> int:
+        
+        m,n = len(heights), len(heights[0])
+        visited = set()
+        minHeap = [(0,0,0)] #diff, row,column
+        
+        while minHeap:
+            diff, r, c = heapq.heappop(minHeap)
+            if (r,c)==(m-1,n-1):
+                return diff
+            if (r,c) in visited:
+                continue
+            visited.add((r,c))
+            for i,j in [[1,0],[-1,0],[0,1],[0,-1]]:
+                rd, cd = r+i,c+j
+                if 0<=rd<m and 0<=cd<n and (rd,cd) not in visited:
+                    newDiff = max(abs(heights[r][c]-heights[rd][cd]), diff)
+                    heapq.heappush(minHeap,(newDiff, rd,cd))
+                    
+        
+        
+            
+        
