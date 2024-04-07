2c8f8cd6c937205e091d182c8550c50bd728b1cc
Adarsh Ramesh Kumar <102381596+a-darsh@users.noreply.github.com>
2024-04-06 19:39:26 -0500

    Time: 493 ms (71.08%), Space: 19.1 MB (26.22%) - LeetHub

--- 1631-path-with-minimum-effort/1631-path-with-minimum-effort.py
+++ 1631-path-with-minimum-effort/1631-path-with-minimum-effort.py
@@ -1,24 +1,27 @@
 class Solution:
     def minimumEffortPath(self, heights: List[List[int]]) -> int:
         
-        m,n = len(heights), len(heights[0])
+        m,n  = len(heights), len(heights[0])
         visited = set()
-        minHeap = [(0,0,0)] #diff, row,column
+        minHeap = [(0,0,0)] #diff, row, col
         
         while minHeap:
             diff, r, c = heapq.heappop(minHeap)
-            if (r,c)==(m-1,n-1):
+            if (r,c) == (m-1,n-1):
                 return diff
+            
             if (r,c) in visited:
                 continue
+            
             visited.add((r,c))
             for i,j in [[1,0],[-1,0],[0,1],[0,-1]]:
-                rd, cd = r+i,c+j
+                rd,cd = r+i, c+j
                 if 0<=rd<m and 0<=cd<n and (rd,cd) not in visited:
-                    newDiff = max(abs(heights[r][c]-heights[rd][cd]), diff)
-                    heapq.heappush(minHeap,(newDiff, rd,cd))
-                    
+                    newDiff = max(diff, abs(heights[r][c]-heights[rd][cd]))
+                    heapq.heappush(minHeap, (newDiff, rd, cd))
         
+            
+            
         
             
         
