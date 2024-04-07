5993873b80cd73999b65295c90b4185da5294f7a
Adarsh Ramesh Kumar <102381596+a-darsh@users.noreply.github.com>
2024-04-07 09:08:29 -0500

    Time: 44 ms (13.20%), Space: 16.6 MB (42.29%) - LeetHub

---
+++ 0075-sort-colors/0075-sort-colors.py
@@ -0,0 +1,23 @@
+class Solution:
+    def sortColors(self, nums: List[int]) -> None:
+        """
+        Do not return anything, modify nums in-place instead.
+        """
+        
+        hmap = defaultdict(int)
+        for i in range(len(nums)):
+            hmap[nums[i]] += 1
+        
+        for i in range(len(nums)):
+            if hmap[0]:
+                nums[i]=0
+                hmap[0]-=1
+            elif hmap[1]:
+                nums[i]=1
+                hmap[1]-=1
+            else:
+                nums[i]=2
+                hmap[2]-=1
+                
+        
+        
