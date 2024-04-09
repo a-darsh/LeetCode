1e7519096ade18a39e3167733649dff22da5dee3
Adarsh Ramesh Kumar <102381596+a-darsh@users.noreply.github.com>
2024-04-09 17:16:08 -0500

    Time: 120 ms (22.97%), Space: 16.6 MB (72.58%) - LeetHub

--- 0459-repeated-substring-pattern/0459-repeated-substring-pattern.py
+++ 0459-repeated-substring-pattern/0459-repeated-substring-pattern.py
@@ -2,12 +2,12 @@ class Solution:
     def repeatedSubstringPattern(self, s: str) -> bool:
         
         l = len(s)
-        if l == 1:
+        
+        if l==1:
             return False
         
-        for i in range(1, l//2 + 1):  # Start from 1 and go up to l//2
-            if l % (i) == 0:  # Check if the length of s is divisible by i
-                if s == (s[:i]) * (l//i):
-                    return True
+        for i in range(1, l//2+1):
+            if s==s[:i]*(l//i):
+                return True
         
         return False
