5b1e1c6133d253f58098890d8d8ba0354655f6db
Adarsh Ramesh Kumar <102381596+a-darsh@users.noreply.github.com>
2024-04-09 08:55:36 -0500

    Time: 757 ms (24.43%), Space: 17.2 MB (19.86%) - LeetHub

--- 2101-detonate-the-maximum-bombs/2101-detonate-the-maximum-bombs.py
+++ 2101-detonate-the-maximum-bombs/2101-detonate-the-maximum-bombs.py
@@ -1,30 +1,35 @@
 class Solution:
     def maximumDetonation(self, bombs: List[List[int]]) -> int:
         
-        def checkInside(b1,b2):
+        def inRange(b1, b2):
             d = (b1[0]-b2[0])**2+(b1[1]-b2[1])**2
             if d>b1[2]**2:
                 return False
             return True
         
         graph = defaultdict(list)
-        for i, b1 in enumerate(bombs):
-            for j, b2 in enumerate(bombs):
-                if checkInside(b1,b2) and i!=j:
+        for i in range(len(bombs)):
+            for j in range(len(bombs)):
+                if i!=j and inRange(bombs[i], bombs[j]):
                     graph[i].append(j)
         
-        maxCount = float('-inf')
+    
         def dfs(i, visited):
             if i in visited:
                 return 0
-            count=1
             visited.add(i)
+            count = 1
             for neigh in graph[i]:
-                count+=dfs(neigh,visited)
+                count += dfs(neigh, visited)
             return count
         
-        for i,_ in enumerate(bombs):
+        maxCount = float('-inf')
+        for i in range(len(bombs)):
             visited = set()
             maxCount = max(maxCount, dfs(i, visited))
+        # for i in graph.keys():
+        #     visited = set()
+        #     maxCount = max(maxCount, dfs(i, visited))
             
         return maxCount
+            
