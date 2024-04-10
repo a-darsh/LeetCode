ff44eec5abf926fdea9f8b87e9a2ea479ca86fc4
Adarsh Ramesh Kumar <102381596+a-darsh@users.noreply.github.com>
2024-04-10 12:59:27 -0500

    Time: 37 ms (75.42%), Space: 16.6 MB (78.52%) - LeetHub

--- 0088-merge-sorted-array/0088-merge-sorted-array.py
+++ 0088-merge-sorted-array/0088-merge-sorted-array.py
@@ -4,22 +4,27 @@ class Solution:
         Do not return anything, modify nums1 in-place instead.
         """
         
-        # Correct calculation of the last index
-        last = m + n - 1
-        m -= 1
-        n -= 1
-
-        while m >= 0 and n >= 0:
-            if nums1[m] > nums2[n]:
+        last = m+n-1
+        m-=1
+        n-=1
+        while m>=0 and n>=0:
+            if nums1[m]>nums2[n]:
                 nums1[last] = nums1[m]
-                m -= 1
+                m-=1
             else:
                 nums1[last] = nums2[n]
-                n -= 1
-            last -= 1
+                n-=1
+            last-=1
         
-        # Copy remaining elements from nums2 if any
-        while n >= 0:
+        while n>=0:
             nums1[last] = nums2[n]
-            n -= 1
-            last -= 1
+            n-=1
+            last-=1
+        
+            
+                
+            
+        
+            
+        
+        
