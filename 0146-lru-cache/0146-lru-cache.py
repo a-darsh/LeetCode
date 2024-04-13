4016b839a3e12773c8e5e1e3da3c29b796b19756
Adarsh Ramesh Kumar <102381596+a-darsh@users.noreply.github.com>
2024-04-12 19:32:15 -0500

    Time: 551 ms (76.24%), Space: 77.8 MB (64.39%) - LeetHub

--- 0146-lru-cache/0146-lru-cache.py
+++ 0146-lru-cache/0146-lru-cache.py
@@ -1,29 +1,65 @@
-from collections import OrderedDict
+class LinkNode:
+    def __init__(self, key, value):
+        self.key = key
+        self.value = value
+        self.next = None
+        self.prev = None
+
 class LRUCache:
 
     def __init__(self, capacity: int):
-        self.cache = OrderedDict()
-        self.size = 0
         self.capacity = capacity
-
+        self.size = 0
+        self.head = LinkNode(-1,-1)
+        self.tail = LinkNode(-1, -1)
+        self.head.next = self.tail
+        self.tail.prev = self.head
+        self.cache = {}
+    
+    def add(self, node):
+        node.prev = self.head
+        node.next = self.head.next
+        node.next.prev = node
+        self.head.next = node
+    
+    def remove(self, node):
+        node.prev.next = node.next
+        node.next.prev = node.prev
+    
+    
     def get(self, key: int) -> int:
         if key not in self.cache:
             return -1
-        val = self.cache.pop(key)
-        self.cache[key] = val
-        return self.cache[key]
+        
+        node = self.cache[key]
+        self.remove(node)
+        self.add(node)
+        
+        return node.value
+        
 
     def put(self, key: int, value: int) -> None:
+        
         if key in self.cache:
-            self.cache.pop(key)
-            self.cache[key] = value
-        elif self.capacity==self.size:
-            self.cache.popitem(last=False)
-            self.cache[key] = value
-        else:
-            self.cache[key] = value
+            node = self.cache[key]
+            self.remove(node)
+            node.value = value
+            self.cache[key] = node
+            self.add(node)
+        
+        elif self.size<self.capacity:
+            node = LinkNode(key, value)
+            self.cache[key] = node
+            self.add(node)
             self.size+=1
-            
+        
+        else:
+            lru = self.tail.prev
+            self.remove(lru)
+            del self.cache[lru.key]
+            node = LinkNode(key, value)
+            self.cache[key] = node
+            self.add(node)
         
 
 
