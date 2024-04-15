7eb2488388989113be71ccf97ae14e18570d5eee
Adarsh Ramesh Kumar <102381596+a-darsh@users.noreply.github.com>
2024-04-14 21:52:20 -0500

    Time: 54 ms (52.87%), Space: 16.6 MB (85.55%) - LeetHub

--- 0039-combination-sum/0039-combination-sum.py
+++ 0039-combination-sum/0039-combination-sum.py
@@ -2,19 +2,15 @@ class Solution:
     def combinationSum(self, candidates: List[int], target: int) -> List[List[int]]:
         
         ans = []
-    
-        def dfs(s, index, cur=[]):
-            if s==target:
-                ans.append(cur)
-                return
-            if s>target:
-                return
-            for i in range(index,len(candidates)):
-                dfs(s+candidates[i], i, cur+[candidates[i]])
         
-        dfs(0,0)
+        def backtrack(start, comb, curTarget):
+            if curTarget==0:
+                ans.append(comb)
+                return 
+            if curTarget<0:
+                return
+            for i,num in enumerate(candidates[start:]):
+                backtrack(start+i, comb+[num], curTarget-num)
         
+        backtrack(0, [],target)
         return ans
-        
-        
-            
