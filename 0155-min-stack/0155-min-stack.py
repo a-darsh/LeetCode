f315f110a20bf7ef25ea5e2e895256657fc1c374
Adarsh Ramesh Kumar <102381596+a-darsh@users.noreply.github.com>
2024-04-23 23:17:08 -0500

    Time: 47 ms (91.50%), Space: 20.2 MB (93.06%) - LeetHub

--- 0155-min-stack/0155-min-stack.py
+++ 0155-min-stack/0155-min-stack.py
@@ -1,24 +1,31 @@
 class MinStack:
 
     def __init__(self):
-        self.lst = []
-        self.min = []
+        self.stack = deque([])
+        self.minList = []
 
     def push(self, val: int) -> None:
-        self.lst.append(val)
-        val = min(val, self.min[-1]) if self.min else val
-        self.min.append(val)
+        self.stack.appendleft(val)
+        if self.minList:
+            if self.minList[-1]>val:
+                self.minList.append(val)
+            else:
+                self.minList.append(self.minList[-1])
+        else:
+            self.minList.append(val)
+        return
 
     def pop(self) -> None:
-        self.lst.pop()
-        self.min.pop()
-        
+        self.stack.popleft()
+        self.minList.pop()
+        return
 
     def top(self) -> int:
-        return self.lst[-1]
+        return self.stack[0]
+        
 
     def getMin(self) -> int:
-      return self.min[-1]
+        return self.minList[-1]
         
 
 
