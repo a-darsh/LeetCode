62102fe52419e686655d68861f0a03721752f3ef
Adarsh Ramesh Kumar <102381596+a-darsh@users.noreply.github.com>
2024-06-23 17:08:07 -0500

    Time: 117 ms (80.14%), Space: 19.6 MB (46.19%) - LeetHub

--- 0127-word-ladder/0127-word-ladder.py
+++ 0127-word-ladder/0127-word-ladder.py
@@ -16,13 +16,13 @@ class Solution:
         visited = set()
         while queue:
             word, level = queue.popleft()
-            visited.add(word)
             if word==endWord:
                 return level
             for i in range(len(word)):
                 pattern = word[:i]+"*"+word[i+1:]
                 for nei in adjList[pattern]:
                     if nei not in visited:
+                        visited.add(nei)
                         queue.append((nei, level+1))
         
         return 0
