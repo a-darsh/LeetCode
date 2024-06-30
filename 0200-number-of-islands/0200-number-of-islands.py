e2fa2c4437e8115a00cb50e78e25edb358c7099a
Adarsh Ramesh Kumar <102381596+a-darsh@users.noreply.github.com>
2024-06-30 13:47:50 -0500

    Time: 274 ms (28.62%), Space: 24.2 MB (25.52%) - LeetHub

--- 0200-number-of-islands/0200-number-of-islands.py
+++ 0200-number-of-islands/0200-number-of-islands.py
@@ -1,20 +1,22 @@
 class Solution:
     def numIslands(self, grid: List[List[str]]) -> int:
+        
         m,n  = len(grid), len(grid[0])
         visited = set()
-        res = 0
         def dfs(i,j):
             if (i,j) in visited:
                 return
             visited.add((i,j))
-            for x,y in [[1,0], [-1,0], [0,1], [0,-1]]:
-                r,c = i+x, j+y
-                if 0<=r<m and 0<=c<n and grid[r][c]=="1":
+            for dr,dc in [[1,0], [-1,0], [0,1], [0,-1]]:
+                r,c = i+dr, j+dc
+                if 0<=r<m and 0<=c<n and grid[r][c]=="1" and (r,c) not in visited:
                     dfs(r,c)
-        for i in range(m):
-            for j in range(n):
-                if (i,j) not in visited and grid[i][j]=="1":
-                    res+=1
-                    dfs(i,j)
-        return res
-                    
+            return
+        
+        islands = 0
+        for row in range(m):
+            for col in range(n):
+                if (row,col) not in visited and grid[row][col]=="1":
+                    islands+=1
+                    dfs(row,col)
+        return islands
