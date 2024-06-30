7f069c36b217e5502df9e2a1ebb599f57e24a1a1
Adarsh Ramesh Kumar <102381596+a-darsh@users.noreply.github.com>
2024-06-30 10:51:22 -0500

    Time: 75 ms (49.64%), Space: 19.8 MB (70.66%) - LeetHub

--- 0253-meeting-rooms-ii/0253-meeting-rooms-ii.py
+++ 0253-meeting-rooms-ii/0253-meeting-rooms-ii.py
@@ -4,18 +4,17 @@ class Solution:
     def minMeetingRooms(self, intervals: List[List[int]]) -> int:
         
         intervals.sort()
-        occupied = []
-        count=0
-        for interval in intervals:
-            if occupied and occupied[0]<=interval[0]:
-                heapq.heappop(occupied)
-                heapq.heappush(occupied, interval[1])
-            else:
-                heapq.heappush(occupied, interval[1])
-                count+=1
-        return count
-        
-        
+        maxRoom = float("-inf")
+        curEnds = []
+        size = 0
+        for meet in intervals:
+            while curEnds and curEnds[0] <= meet[0]:
+                heapq.heappop(curEnds)
+                size-=1
+            heapq.heappush(curEnds, meet[1])
+            size+=1
+            maxRoom = max(maxRoom, size)
+        return maxRoom
         
             
         
