f7120246d11c4033c2e22a82d9dcb30dd3617e11
Adarsh Ramesh Kumar <102381596+a-darsh@users.noreply.github.com>
2024-06-30 22:53:18 -0500

    Time: 460 ms (35.63%), Space: 24.6 MB (25.90%) - LeetHub

--- 1293-shortest-path-in-a-grid-with-obstacles-elimination/1293-shortest-path-in-a-grid-with-obstacles-elimination.py
+++ 1293-shortest-path-in-a-grid-with-obstacles-elimination/1293-shortest-path-in-a-grid-with-obstacles-elimination.py
@@ -2,30 +2,24 @@ class Solution:
     def shortestPath(self, grid: List[List[int]], k: int) -> int:
         
         m,n  = len(grid), len(grid[0])
-        visited = set()
-        
-        queue = deque([(0,k,0,0)]) #steps,obstacle-remain, row, column
-        target = (m-1,n-1)
-        
+        visited = set() #obst, row, col
+        queue = deque([(0,k,0,0)]) #steps, rem obst, row, col
+        visited.add((0,0,0))
         while queue:
-            
-            steps,obst, r,c  = queue.popleft() 
-            if (r,c)==target:
+            steps, obst, r, c = queue.popleft()
+            if (r,c)==(m-1,n-1):
                 return steps
-            
-            visited.add((obst,r,c))
-            for i,j in [[1,0], [-1,0], [0,1], [0,-1]]:
-                rd, cd = r+i, c+j
-                if 0<=rd<m and 0<=cd<n:
-                    if grid[rd][cd]==1 and obst >0 and (obst-1,rd,cd) not in visited:
-                        visited.add((obst-1,rd,cd))
-                        queue.append((steps+1, obst-1, rd, cd))
-                    elif grid[rd][cd]!=1 and (obst,rd,cd) not in visited:
-                        visited.add((obst,rd,cd))
-                        queue.append((steps+1, obst, rd, cd))
-            
+            for rd, cd in [[1,0],[-1,0],[0,1],[0,-1]]:
+                nr,nc = r+rd, c+cd
+                if 0<=nr<m and 0<=nc<n:
+                    if grid[nr][nc]==1 and obst>0 and (obst-1, nr,nc) not in visited:
+                        visited.add((obst-1, nr,nc))
+                        queue.append((steps+1, obst-1, nr,nc))
+                    if grid[nr][nc]!=1 and (obst, nr,nc) not in visited:
+                        visited.add((obst, nr,nc))
+                        queue.append((steps+1, obst, nr,nc))
         return -1
-            
+         
             
         
         
