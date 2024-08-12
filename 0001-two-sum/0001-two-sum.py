842f3bed72f5bb9a8d14f7f67da1036286e7e6be
Adarsh Ramesh Kumar <102381596+a-darsh@users.noreply.github.com>
2024-08-11 19:06:43 -0500

    Time: 64 ms (48.68%), Space: 17.8 MB (27.33%) - LeetHub

--- 0001-two-sum/0001-two-sum.py
+++ 0001-two-sum/0001-two-sum.py
@@ -3,7 +3,7 @@ class Solution:
         
         hmap = {}
         for i, num in enumerate(nums):
-            if target-num in hmap:
-                return [hmap[(target-num)], i]
-            hmap[num] = i
-        
+            dif = target-num
+            if dif in hmap:
+                return [hmap[dif],i]
+            hmap[num]=i
