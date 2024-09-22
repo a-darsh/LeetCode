1eff1a25fa35a4181d8da1ad34c24a8f048a36b0
Adarsh Ramesh Kumar <102381596+a-darsh@users.noreply.github.com>
2024-09-22 00:46:44 -0500

    Time: 268 ms (32.04%), Space: 24.4 MB (23.89%) - LeetHub

--- 0200-number-of-islands/0200-number-of-islands.py
+++ 0200-number-of-islands/0200-number-of-islands.py
@@ -1,22 +1,26 @@
 class Solution:
     def numIslands(self, grid: List[List[str]]) -> int:
         
-        m,n  = len(grid), len(grid[0])
+        m,n = len(grid), len(grid[0])
         visited = set()
+        islands = 0
+        
         def dfs(i,j):
-            if (i,j) in visited:
-                return
-            visited.add((i,j))
-            for dr,dc in [[1,0], [-1,0], [0,1], [0,-1]]:
-                r,c = i+dr, j+dc
-                if 0<=r<m and 0<=c<n and grid[r][c]=="1" and (r,c) not in visited:
-                    dfs(r,c)
+            if (i,j) not in visited and grid[i][j]=="1":
+                visited.add((i,j))
+                for di, dj in [[1,0], [-1,0], [0,1], [0,-1]]:
+                    r,c = i+di, j+dj
+                    if 0<=r<m and 0<=c<n and grid[r][c]=="1":
+                        dfs(r,c)
             return
         
-        islands = 0
-        for row in range(m):
-            for col in range(n):
-                if (row,col) not in visited and grid[row][col]=="1":
+        for i in range(m):
+            for j in range(n):
+                if (i,j) not in visited and grid[i][j]=="1":
                     islands+=1
-                    dfs(row,col)
+                    dfs(i,j)
+        
         return islands
+        
+        
+        
