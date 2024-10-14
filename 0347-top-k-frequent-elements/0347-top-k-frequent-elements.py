c282dca968a8e8140753521988074deacaf6ceb6
Adarsh Ramesh Kumar <102381596+a-darsh@users.noreply.github.com>
2024-10-13 21:42:36 -0500

    Time: 90 ms (50.07%), Space: 21.2 MB (45.57%) - LeetHub

--- 0347-top-k-frequent-elements/0347-top-k-frequent-elements.py
+++ 0347-top-k-frequent-elements/0347-top-k-frequent-elements.py
@@ -1,14 +1,16 @@
 class Solution:
     def topKFrequent(self, nums: List[int], k: int) -> List[int]:
+        
         hmap = defaultdict(int)
-        for i in nums:
-            hmap[i]+=1
-            
+        for n in nums:
+            hmap[n]+=1
+        
         maxHeap = []
         for key,val in hmap.items():
-            heapq.heappush(maxHeap, (-val,key))
+            heapq.heappush(maxHeap, (-val, key))
         
         ans = []
         for i in range(k):
             ans.append(heapq.heappop(maxHeap)[1])
+        
         return ans
