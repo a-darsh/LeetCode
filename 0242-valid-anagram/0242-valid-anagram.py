cae899cd62aa7a5865fd28100eded642db9d9230
Adarsh Ramesh Kumar <102381596+a-darsh@users.noreply.github.com>
2024-12-21 13:42:10 -0600

    Time: 11 ms (75.24%), Space: 17.9 MB (10.45%) - LeetHub

--- 0242-valid-anagram/0242-valid-anagram.py
+++ 0242-valid-anagram/0242-valid-anagram.py
@@ -3,11 +3,18 @@ class Solution:
         
         if len(s)!=len(t):
             return False
+        
         hmap1, hmap2 = defaultdict(int), defaultdict(int)
         for i in range(len(s)):
             hmap1[s[i]]+=1
             hmap2[t[i]]+=1
+        
         for k,v in hmap1.items():
-            if v!=hmap2[k]:
+            if k in hmap2:
+                if hmap2[k]!=v:
+                    return False
+            else:
                 return False
         return True
+    
+    #TC: O(n), SC: O(n)
