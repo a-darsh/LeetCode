9614c0cf95e5aaea5e4cc549630aab2f27fe96e8
Adarsh Ramesh Kumar <102381596+a-darsh@users.noreply.github.com>
2024-12-22 10:57:37 -0600

    Time: 0 ms (100.00%), Space: 19 MB (5.05%) - LeetHub

--- 0001-two-sum/0001-two-sum.py
+++ 0001-two-sum/0001-two-sum.py
@@ -2,8 +2,10 @@ class Solution:
     def twoSum(self, nums: List[int], target: int) -> List[int]:
         
         hmap = {}
-        for i, num in enumerate(nums):
-            dif = target-num
-            if dif in hmap:
-                return [hmap[dif],i]
-            hmap[num]=i
+        for i, n in enumerate(nums):
+            rem = target - n
+            if rem in hmap:
+                return [hmap[rem],i]
+            hmap[n]=i
+        
+        # TC: O(n), SC: O(n)
