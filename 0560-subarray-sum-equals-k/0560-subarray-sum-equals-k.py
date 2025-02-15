83ecf822f1903a70ba89426e8dfa65bfe5c3b7b4
Adarsh Ramesh Kumar <102381596+a-darsh@users.noreply.github.com>
2025-02-15 13:19:22 -0600

    Time: 37 ms (38.95%), Space: 20.3 MB (68.74%) - LeetHub

--- 0560-subarray-sum-equals-k/0560-subarray-sum-equals-k.py
+++ 0560-subarray-sum-equals-k/0560-subarray-sum-equals-k.py
@@ -1,16 +1,13 @@
 class Solution:
     def subarraySum(self, nums: List[int], k: int) -> int:
-        
-        hmap = defaultdict(int) #prefixsum:numbre of ways
-        hmap[0] = 1
-        cur = 0
-        ans = 0
-        for i in range(len(nums)):
-            cur = cur+nums[i]
-            if cur-k in hmap:
-                ans+=hmap[cur-k]
-            hmap[cur]+=1
-        
-        return ans
-            
-            
+        hmap = defaultdict(int)
+        hmap[0]+=1
+        res = 0
+        curSum = 0
+        for n in nums:
+            curSum += n
+            checkSum = curSum-k
+            if checkSum in hmap:
+                res+=hmap[checkSum]
+            hmap[curSum]+=1
+        return res
