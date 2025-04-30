ee967874c30008171b997882d85e83d0d8bc1acb
Adarsh Ramesh Kumar <102381596+a-darsh@users.noreply.github.com>
2025-04-30 00:33:10 -0500

    Time: 0 ms (100%), Space: 17.7 MB (88.55%) - LeetHub

--- 0408-valid-word-abbreviation/0408-valid-word-abbreviation.py
+++ 0408-valid-word-abbreviation/0408-valid-word-abbreviation.py
@@ -2,19 +2,21 @@ class Solution:
     def validWordAbbreviation(self, word: str, abbr: str) -> bool:
         i,j = 0,0
         while i<len(word) and j<len(abbr):
-            if abbr[j]=='0':
-                return False
             if abbr[j].isdigit():
+                if abbr[j]=='0':
+                    return False
                 skip=0
                 while j<len(abbr) and abbr[j].isdigit():
-                    skip=skip*10+ int(abbr[j])
+                    skip=skip*10+int(abbr[j])
                     j+=1
                 i+=skip
+            elif word[i]!=abbr[j]:
+                return False
             else:
-                if abbr[j]!=word[i]:
-                    return False
                 i+=1
                 j+=1
         return i==len(word) and j==len(abbr)
 
-        #O(N), O(1)
+    #O(N), O(1)
+
+                
