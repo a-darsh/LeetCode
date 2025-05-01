5d9e5316109505c8690be82ea70521563360e2c2
Adarsh Ramesh Kumar <102381596+a-darsh@users.noreply.github.com>
2025-04-30 23:47:54 -0500

    Time: 0 ms (100%), Space: 17.7 MB (67.89%) - LeetHub

--- 0314-binary-tree-vertical-order-traversal/0314-binary-tree-vertical-order-traversal.py
+++ 0314-binary-tree-vertical-order-traversal/0314-binary-tree-vertical-order-traversal.py
@@ -8,19 +8,16 @@ class Solution:
     def verticalOrder(self, root: Optional[TreeNode]) -> List[List[int]]:
         if not root:
             return []
+            
         hmap = defaultdict(list)
-        queue = deque([(0,root)])
-        minX, maxX = float('inf'), float('-inf')
-        while queue:
-            x,node = queue.popleft()
-            minX = min(minX, x)
-            maxX = max(maxX, x)
-            hmap[x].append(node.val)
+        q = deque([(root,0)])
+        while q:
+            node, col = q.popleft()
+            hmap[col].append(node.val)
             if node.left:
-                queue.append((x-1, node.left))
+                q.append((node.left, col-1))
             if node.right:
-                queue.append((x+1,node.right))
-        return [hmap[x] for x in range(minX, maxX+1)]
-
-
-        
+                q.append((node.right, col+1))
+        return [hmap[k] for k in sorted(hmap.keys())]
+    
+    #O(N), O(N)
